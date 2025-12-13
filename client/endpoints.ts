aa95599de93fe8ca1ab8510c7f8bda30469a6de7
jayellho <jayellho@gmail.com>
2025-12-13 15:50:57 -0800

    chore: console.log.

--- client/endpoints.ts
+++ client/endpoints.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const BACKEND_URL = process.env.BACKEND_URL; // e.g. "https://your-ec2-domain.com"
-
+console.log(BACKEND_URL)
 export const triggerAtgScrape = async () => {
   const response = await axios.post(`${BACKEND_URL}/api/atg/scrape`, {
     headless: true,
