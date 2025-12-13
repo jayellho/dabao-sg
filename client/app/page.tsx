bb9cf8dc66d149b0fd0dd2a342d6e6c34ef6e78f
jayellho <jayellho@gmail.com>
2025-12-13 14:36:01 -0800

    chore: test update frontend.

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,14 +1,20 @@
 import Image from "next/image";
 import { Button, Flex } from 'antd';
+import { getScrapeStatus } from "../endpoints"
 
-export default function Home() {
+
+const Home = () => {
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Flex gap="small" wrap>
-          <Button type="primary">Update Caterings</Button>
+          <Button type="primary" onClick={() => getScrapeStatus()}>Update Caterings</Button>
+          <Button type="primary" onClick={() => getScrapeStatus()}>Test</Button>
         </Flex>
       </main>
     </div>
   );
 }
+
+export default Home;
