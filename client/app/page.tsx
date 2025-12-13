90304b9642357d7ccc3482193be7efc29cfedaac
jayellho <jayellho@gmail.com>
2025-12-13 15:38:16 -0800

    feat: add link to endpoint for scraping ATG on the frontend.

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Button, Flex } from 'antd';
-import { getScrapeStatus } from "../endpoints"
+import { triggerAtgScrape } from "../endpoints"
 
 
 const Home = () => {
@@ -9,8 +9,15 @@ const Home = () => {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Flex gap="small" wrap>
-          <Button type="primary" onClick={() => getScrapeStatus()}>Update Caterings</Button>
-          <Button type="primary" onClick={() => getScrapeStatus()}>Test</Button>
+          <Button
+            type="primary"
+            onClick={async () => {
+              const res = await triggerAtgScrape();
+              console.log(res);
+            }}
+          >
+            Update Caterings
+          </Button>
         </Flex>
       </main>
     </div>
--- client/endpoints.ts
+++ client/endpoints.ts
@@ -1,15 +1,13 @@
 import axios from 'axios';
 
 
-export const getScrapeStatus = async () => {
-  try {
-    const result: any = [];
-    const response = await axios.post('/api/atg/scrape');
-    const { decoding } = await response.data;
-    
-    return result;
-  } catch (error) {
-    console.log(error);
-    return [];
-  }
+const BACKEND_URL = process.env.BACKEND_URL; // e.g. "https://your-ec2-domain.com"
+
+export const triggerAtgScrape = async () => {
+  const response = await axios.post(`${BACKEND_URL}/api/atg/scrape`, {
+    headless: true,
+    max_orders: 200,
+    sync_calendar: true,
+  });
+  return response.data;
 };
